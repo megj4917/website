1e6e38b01b16497a666e95bd59b7fc1d57f95bce
megj4917 <59276924+megj4917@users.noreply.github.com>
2020-03-15 15:46:15 -0700

    Update send.php

--- send.php
+++ send.php
@@ -3,6 +3,8 @@
 	$email = $_POST['email'];
 	$message = $_POST['message'];
 
+	$email_from = 'meganjacocb2004@yahoo.com';
+
 	$email_subject = "New Contact Form Submission";
 
 	$email_body = "Name: $name.\n".
@@ -11,6 +13,10 @@
 
 	$email_to = 'meganjacob2004@yahoo.com';
 
-	mail($email_to, $email_subject, $email_body);
+	$headers = "From: $email_from \r\n";
+	$headers = "Reply-To: $visitor_email \r\n";
+
+	mail($email_to,$email_subject,$email_body, $headers);
+
 	header("Location: contact.html");
 ?>
