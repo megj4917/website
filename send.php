1c55b435f35a112de9d3458b7a53b5603175de3a
megj4917 <59276924+megj4917@users.noreply.github.com>
2020-03-15 15:37:37 -0700

    Create send.php

---
+++ send.php
@@ -0,0 +1,16 @@
+<?php
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+
+	$email_subject = "New Contact Form Submission";
+
+	$email_body = "Name: $name.\n".
+				  "Email: $email.\n".
+				  "Message: $message.\n";
+
+	$email_to = 'meganjacob2004@yahoo.com';
+
+	mail($to, $email_subject, $email_body);
+	header("Location: contact.html");
+?>
