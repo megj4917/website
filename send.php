ee7c8b985436cc7c42f28e12a617b5150a42c73d
megj4917 <59276924+megj4917@users.noreply.github.com>
2020-03-15 15:50:25 -0700

    Update send.php

--- send.php
+++ send.php
@@ -1,6 +1,6 @@
 <?php
 	$name = $_POST['name'];
-	$email = $_POST['email'];
+	$visitor_email = $_POST['email'];
 	$message = $_POST['message'];
 
 	$email_from = 'meganjacocb2004@yahoo.com';
@@ -14,7 +14,7 @@
 	$email_to = 'meganjacob2004@yahoo.com';
 
 	$headers = "From: $email_from \r\n";
-	$headers = "Reply-To: $visitor_email \r\n";
+	$headers .= "Reply-To: $visitor_email \r\n";
 
 	mail($email_to,$email_subject,$email_body, $headers);
 
